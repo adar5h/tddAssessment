177a61d7759819f6b0a992f5b3880f7b35442ec1
“adar5h” <iadarshh@gmail.com>
2025-05-08 01:02:46 +0530

    improvised code

--- kata_calculator.rb
+++ kata_calculator.rb
@@ -5,21 +5,8 @@ class KataCalculator
 
     nums.each do |num|
       if num.is_a?(String)
-        delimiter = nil
-        numbers_string = num
-
-        if num.start_with?('//')
-          delimiter_line, numbers_string = num.split("\n", 2)
-          delimiter = delimiter_line[2..] # Removed the \\ part
-        end
-
-        numbers = if delimiter
-                    numbers_string.split(delimiter)
-                  else
-                    num.split(/[\n,]/)
-                  end
-
-        all_nums.concat(numbers.map(&:to_i))
+        nums = delimiter_specified?(num)
+        all_nums.concat(nums)
       else
         # Default delimiter ','
         all_nums << num.to_i
@@ -29,5 +16,21 @@ class KataCalculator
     all_nums.sum
   end
 
-  
+  def self.delimiter_specified?(num)
+    delimiter = nil
+    numbers_string = num
+
+    if num.start_with?('//')
+      delimiter_line, numbers_string = num.split("\n", 2)
+      delimiter = delimiter_line[2..] # Removed the \\ part
+    end
+
+    numbers = if delimiter
+                numbers_string.split(delimiter)
+              else
+                num.split(/[\n,]/)
+              end
+
+    numbers.map(&:to_i)
+  end
 end
