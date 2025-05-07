be7efd28f816ae204f4a3dc9780671175a6753a1
“adar5h” <iadarshh@gmail.com>
2025-05-08 00:52:36 +0530

    refactored code for , and \n delimiters

--- kata_calculator.rb
+++ kata_calculator.rb
@@ -1,8 +1,19 @@
 # Kata Calculator
 class KataCalculator
   def self.add(*nums)
-    nums = nums.map(&:to_i)
+    all_nums = []
 
-    nums.sum
+    nums.each do |num|
+      if num.is_a?(String)
+        # Split by newlines and commas
+        numbers = num.split(/[\n,]/)
+        all_nums.concat(numbers.map(&:to_i))
+      else
+        # Default delimiter ','
+        all_nums << num.to_i
+      end
+    end
+
+    all_nums.sum
   end
 end
